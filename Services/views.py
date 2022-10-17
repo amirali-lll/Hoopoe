e135bcdffabf77c37d4b6bb8e1c54dd7922ff19d
amirali-lll <amirali.dst.lll@gmail.com>
2022-10-18 00:37:08 +0330

    create new template for render services dynamicly.

--- Services/views.py
+++ Services/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from Services.models import Service
 
 # Create your views here.
+def dynamic_lookup(request,id):
+    service = Service.objects.get(id=id)
+    all_services = Service.objects.all()
+
+    context = {
+        'service':service,
+        'all_services':all_services
+    }
+    return render(request,'service.html',context)
