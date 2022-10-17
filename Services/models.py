5c8bf975913608873b37e5fcbeffb79c3ea9d52f
amirali-lll <amirali.dst.lll@gmail.com>
2022-10-17 16:05:08 +0330

    Add services fileds.

--- Services/models.py
+++ Services/models.py
@@ -1,7 +1,23 @@
-from unicodedata import name
 from django.db import models
 
 # Create your models here.
 class Service(models.Model):
     name = models.CharField(max_length=30)
-    image = models.ImageField()
+
+    feature_summery1 = models.CharField(max_length=300,blank=True)
+    feature_summery2 = models.CharField(max_length=300,blank=True)
+    feature_summery3 = models.CharField(max_length=300,blank=True)
+    feature_summery4 = models.CharField(max_length=300,blank=True)
+
+    feature1_title = models.CharField(max_length=30,blank=True)
+    feature1_description = models.TextField(blank=True)
+    feature1_image = models.ImageField(blank=True)
+    
+    feature2_title = models.CharField(max_length=30,blank=True)
+    feature2_description = models.TextField(blank=True)
+    feature2_image = models.ImageField(blank=True)
+
+    feature3_title = models.CharField(max_length=30,blank=True)
+    feature3_description = models.TextField(blank=True)
+    feature3_image = models.ImageField(blank=True)
+
