5c6de24ac8ceedce1b32b5027b608fb283831e64
amirali-lll <amirali.dst.lll@gmail.com>
2022-10-18 00:46:20 +0330

    update nav items into dynamic objects.

--- Services/views.py
+++ Services/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from Services.models import Service
 
 # Create your views here.
 def dynamic_lookup(request,id):
     service = Service.objects.get(id=id)
-    all_services = Service.objects.all()
+    service = get_object_or_404(Service,id=id)
+    services = Service.objects.all()
 
     context = {
         'service':service,
-        'all_services':all_services
+        'services':services
     }
     return render(request,'service.html',context)
