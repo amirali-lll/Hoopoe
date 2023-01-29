8f6dfce35526d6c66284c14405f70207631c8dbb
amirali-lll <amirali.dst.lll@gmail.com>
2023-01-29 22:25:41 +0330

    change model and add overvie title to service model remove it from overviews class

--- Services/models.py
+++ Services/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Service(models.Model):
-    name = models.CharField(max_length=30,primary_key=True)
-    title = models.CharField(max_length=300,blank=True)
+    name = models.CharField(max_length=32,primary_key=True)
+    title = models.CharField(max_length=256,blank=True)
+    overview_title = models.CharField(max_length=256)
     description = models.TextField(blank=True)
     # features | FK
     # overviews | FK
@@ -20,6 +21,5 @@ class Feature(models.Model):
 
 class ServiceOverview(models.Model):
     service = models.ForeignKey(Service,on_delete=models.CASCADE,related_name='overviews')
-    title = models.CharField(max_length=256) 
     text = models.CharField(max_length=512)
 
